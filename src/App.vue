b098e557b58553fcf5b31bc9ad752bbeb1a2bcf3
superxsy <135094999+superxsy@users.noreply.github.com>
2025-07-11 17:48:37 +0800

    feat: manage auto backup interval

--- src/App.vue
+++ src/App.vue
@@ -365,6 +365,7 @@ export default {
     return {
       showBackupModal: false,
       autoBackup: false,
+      autoBackupInterval: null,
       backupHistory: [],
       lastBackupTime: '无',
       currentDate: new Date(),
@@ -1242,6 +1243,9 @@ export default {
     this.loadBackupHistory();
     this.loadThemeSettings();
     this.loadTasks();
+  },
+  beforeUnmount() {
+    clearInterval(this.autoBackupInterval);
   }
 };
 </script>
